448d52382f308912b4f5c1c937fae5828e5745a7
Michal <mszyszka223@gmail.com>
2018-08-04 09:13:22 +0200

    Some changes

---
+++ script.js
@@ -0,0 +1,11 @@
+var fakePhotoDiv = document.getElementsByClassName('fake-photo');
+
+
+//getElementByClassName returns html collection so I need point element like fakePhotoDiv[0]
+fakePhotoDiv[0].addEventListener('click', function(){
+    console.log('fazor');
+});
+
+
+
+
